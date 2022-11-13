70ac72f61a24222f2c7465353799fa768ad8a5db
Matias Abuin <matias.abuin17@gmail.com>
2022-11-13 20:22:30 -0300

    mercado pago sin funcionar

--- controller/InfoneteController.php
+++ controller/InfoneteController.php
@@ -1,5 +1,22 @@
 <?php
 
+// SDK de Mercado Pago
+require 'vendor/autoload.php';
+
+// Agrega credenciales
+MercadoPago\SDK::setAccessToken('TEST-1735674503019884-032202-d54a673969e467b857cf50292aaedaa6-201226814');
+
+// Crea un objeto de preferencia
+$preference = new MercadoPago\Preference();
+
+// Crea un ítem en la preferencia
+$item = new MercadoPago\Item();
+$item->title = 'Mi producto';
+$item->quantity = 1;
+$item->unit_price = 75.56;
+$preference->items = array($item);
+$preference->save();
+
 class InfoneteController {
 
     private $renderer;
