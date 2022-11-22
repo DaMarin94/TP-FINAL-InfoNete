12d3ee2051fc3e0c72a492d4b3e87602b99a94a7
Matias Abuin <matias.abuin17@gmail.com>
2022-11-21 21:37:13 -0300

    biblioteca lector

--- controller/LectorController.php
+++ controller/LectorController.php
@@ -10,53 +10,27 @@ class LectorController{
         $this->renderer = $render;
         $this->model = $model;
         $this->pdfGenerator = $pdfGenerator;
-    }
 
-    public function validarRol(){
         if(!Router::checkAuth([1])){
             Redirect::redirect('/');
         };
     }
 
     public function list(){
-        $this->validarRol();
-
-        $this->misProductos();
+        $this->biblioteca();
     }
 
-    public function misProductos(){
-        $this->validarRol();
-
-        $usuario = $_SESSION['id_user'];
-        $data['listaProductosSuscrito'] = $this->model->getProductosSuscrito($usuario);
-        $data['listaProductosCompras'] = $this->model->getProductosCompras($usuario);
-
-        $data['productos'] = true;
-
-        $data['listaProductos'] = array_unique(array_merge($data['listaProductosSuscrito'], $data['listaProductosCompras']),0);
-
-        $this->renderer->render('lector.mustache', $data);
-    }
-
-    public function producto(){
-        $this->validarRol();
-
-        $idProducto = $_GET['id'];
+    public function biblioteca(){
         $usuario = $_SESSION['id_user'];
 
-        $data['producto'] = $this->model->getProductoPorId($idProducto);
-        $data['listaEdicionesSuscrito'] = $this->model->getEdicionesSuscrito($usuario, $idProducto);
-        $data['listaEdicionesCompradas'] = $this->model->getEdicionesCompradas($usuario, $idProducto);
-
-        $data ['ediciones'] = true;
-        $data['listaEdiciones'] = array_unique(array_merge($data['listaEdicionesCompradas'], $data['listaEdicionesSuscrito']),0);
+        $data['biblioteca'] = true;
+        $data['edicionesSuscrito'] = $this->model->getEdicionesSuscrito($usuario);
+        $data['edicionesCompradas'] = $this->model->getEdicionesCompradas($usuario);
 
         $this->renderer->render('lector.mustache', $data);
     }
 
     public function misPagos(){
-        $this->validarRol();
-
         $usuario = $_SESSION['id_user'];
 
         $data['compras'] = true;
@@ -66,26 +40,23 @@ class LectorController{
     }
 
     public function getPdfSuscripciones() {
-        $this->validarRol();
+        $usuario = $_SESSION['id_user'];
 
         $fechaInicio = $_GET['fechaInicio'];
         $fechaFin = $_GET['fechaFin'];
 
-        $usuario = $_SESSION['id_user'];
         $data['listaSuscripciones'] = $this->model->getSuscripcionesPDF($usuario, $fechaInicio, $fechaFin);
-
 
         $html = $this->renderer->getHtml('reportesPdf/templatePdfLectorSuscripciones.mustache', $data);
         $this->pdfGenerator->generarPdf($html, 'portrait', 'reporte-suscripciones');
     }
 
     public function getPdfCompras() {
-        $this->validarRol();
+        $usuario = $_SESSION['id_user'];
 
         $fechaInicio = $_GET['fechaInicio'];
         $fechaFin = $_GET['fechaFin'];
 
-        $usuario = $_SESSION['id_user'];
         $data['listaCompras'] = $this->model->getComprasPDF($usuario, $fechaInicio, $fechaFin);
 
         $html = $this->renderer->getHtml('reportesPdf/templatePdfLectorCompras.mustache', $data);
--- model/LectorModel.php
+++ model/LectorModel.php
@@ -8,59 +8,33 @@ class LectorModel
         $this->database = $database;
     }
 
-    public function getProductosSuscrito($usuario){
-        $sql = "SELECT p.id, p.portada FROM suscripcion s JOIN usuarios u ON s.usuario_id = u.id
-                                            JOIN producto p ON s.producto_id = p.id
-                                            WHERE u.id = '$usuario'";
-        return $this->database->query($sql);
-    }
-
-    public function getProductosCompras($usuario){
-        $sql = "SELECT p.id, p.portada FROM compra c JOIN edicion e ON c.edicion_id = e.id
-                                        JOIN producto p ON e.producto = p.id
-                                        WHERE c.usuario_id = '$usuario'";
-        return $this->database->query($sql);
-    }
-
-    public function getEdicionesSuscrito($usuario, $idProducto){
-        $sql = "SELECT e.id, e.portada, e.edicion FROM edicion e JOIN producto p on e.producto = p.id 
+    public function getEdicionesSuscrito($usuario){
+        $sql = "SELECT e.id, e.portada, e.edicion, e.producto FROM edicion e JOIN producto p on e.producto = p.id 
                                         JOIN suscripcion s ON s.producto_id = p.id
                                         WHERE s.usuario_id = '$usuario'
-                                        AND s.producto_id = '$idProducto'
-                                        AND e.fecha BETWEEN s.fechaAdquirido AND s.fechaVencimiento";
+                                        AND e.fecha BETWEEN s.fechaAdquirido AND s.fechaVencimiento
+                GROUP BY e.id, e.portada, e.edicion, e.producto";
         return $this->database->query($sql);
     }
 
-    public function getEdicionesCompradas($usuario, $idProducto){
-
-        $sql = "SELECT e.id, e.portada, e.edicion FROM Edicion e
-                WHERE e.producto = '$idProducto'
-                AND EXISTS (SELECT 1
+    public function getEdicionesCompradas($usuario){
+        $sql = "SELECT e.id, e.portada, e.edicion, e.producto FROM Edicion e
+                WHERE EXISTS (SELECT 1
                                 FROM compra c
                                 WHERE c.edicion_id = e.id
-                                AND c.usuario_id = '$usuario')";
+                                AND c.usuario_id = '$usuario')
+                GROUP BY e.id, e.portada, e.edicion, e.producto";
         return $this->database->query($sql);
     }
 
     public function getSuscripciones($usuario){
-
         $sql = "SELECT p.nombre ,s.fechaAdquirido, s.fechaVencimiento, p.precio FROM suscripcion s JOIN usuarios u on s.usuario_id = u.id
                                             JOIN producto p ON s.producto_id = p.id
                                             WHERE u.id = '$usuario'";
         return $this->database->query($sql);
     }
 
-    public function getCompras($usuario){
-
-        $sql = "SELECT p.nombre as nombre, e.edicion, c.fecha, e.precio FROM compra c JOIN usuarios u on c.usuario_id = u.id
-                                        JOIN edicion e ON c.edicion_id = e.id
-                                        JOIN producto p ON p.id = e.producto
-                                        WHERE u.id = '$usuario'";
-        return $this->database->query($sql);
-    }
-
     public function getSuscripcionesPDF($usuario, $fechaInicio, $fechaFin){
-
         $sql = "SELECT p.nombre ,s.fechaAdquirido, s.fechaVencimiento, p.precio FROM suscripcion s JOIN usuarios u on s.usuario_id = u.id
                                             JOIN producto p ON p.id = s.producto_id
                                             WHERE u.id = '$usuario'
@@ -69,35 +43,20 @@ class LectorModel
         return $this->database->query($sql);
     }
 
-    public function getComprasPDF($usuario, $fechaInicio, $fechaFin){
+    public function getCompras($usuario){
+        $sql = "SELECT p.nombre as nombre, e.edicion, c.fecha, e.precio FROM compra c JOIN usuarios u on c.usuario_id = u.id
+                                        JOIN edicion e ON c.edicion_id = e.id
+                                        JOIN producto p ON p.id = e.producto
+                                        WHERE u.id = '$usuario'";
+        return $this->database->query($sql);
+    }
 
+    public function getComprasPDF($usuario, $fechaInicio, $fechaFin){
         $sql = "SELECT p.nombre as nombre, e.edicion, c.fecha, e.precio FROM compra c JOIN usuarios u on c.usuario_id = u.id
                                         JOIN edicion e ON c.edicion_id = e.id
                                         JOIN producto p ON p.id = e.producto
                                         WHERE u.id = '$usuario'
                                         AND c.fecha BETWEEN '$fechaInicio' AND '$fechaFin'";
-        return $this->database->query($sql);
-    }
-
-    public function getNoticiasSuscrito($usuario){
-
-        $sql = "SELECT * FROM contenido c JOIN edicion_seccion_noticia esn on esn.noticia = c.id
-                                        JOIN edicion e ON e.id = esn.edicion
-                                        JOIN producto p ON p.id = e.producto 
-                                        JOIN suscripcion s ON s.producto_id = p.id
-                                        JOIN usuarios u ON u.id = s.usuario_id
-                                        WHERE u.id = '$usuario'
-                                        AND e.fecha BETWEEN s.fechaAdquirido AND s.fechaVencimiento";
-        return $this->database->query($sql);
-    }
-
-    public function getNoticiasCompradas($usuario){
-
-        $sql = "SELECT DISTINCT * FROM contenido c JOIN edicion_seccion_noticia esn on esn.noticia = c.id
-                                        JOIN edicion e ON e.id = esn.edicion
-                                        JOIN compra co ON co.edicion_id = e.id
-                                        JOIN usuarios u ON u.id = co.usuario_id
-                                        WHERE u.id = '$usuario'";
         return $this->database->query($sql);
     }
 
